c854e6ce6ab10d76a6327fa560c2dce0e84b57e3
Vinsyahputra <vinthecyborg@gmail.com>
2020-12-12 18:14:30 +0700

    Menginstall Komponen yang digunakan

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 <meta name="viewport" content="initial-scale=1, width=device-width">
 <meta charset="UTF-8">
-<link rel="stylesheet" type="text/css" href="">
+<link rel="stylesheet" type="text/css" href="public/build/tailwind.css">
 <title>Vin. Site</title>
 </head>
 <body>
