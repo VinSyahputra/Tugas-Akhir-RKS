028efc2852b0a65eed2d91e7a8df2475ff4ba31c
Vinsyahputra <vinthecyborg@gmail.com>
2020-12-12 17:21:16 +0700

    Menginstall Component Awal Project

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta name="viewport" content="initial-scale=1, width=device-width">
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="">
+<title>Vin. Site</title>
+</head>
+<body>
+
+
+
+<script></script>
+</body>
+</html>
