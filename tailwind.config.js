62f7972ac2e88d08f33de13c7aba352920e85023
Vinsyahputra <vinthecyborg@gmail.com>
2020-12-13 04:23:47 +0700

    Reconfigure Taliwind Config JS

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,15 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    colors:{
-      'primary' : '#E5F1F2',
-      'secondary' : '#3C6AE1',
-      'dark' : '#333333',
-      'white' : '#fefefe',
+    extend: {
+      colors:{
+        'primary' : '#E5F1F2',
+        'secondary' : '#3C6AE1',
+        'dark' : '#333333',
+        'white' : '#fefefe',
+      },
     },
+    
   },
   variants: {
     extend: {},
