4565e9f13e5dd988df910eec89a8292d3c09c436
Vinsyahputra <vinthecyborg@gmail.com>
2020-12-13 03:38:14 +0700

    Tambahan Component & .gitignore

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors:{
+      'primary' : '#E5F1F2',
+      'secondary' : '#3C6AE1',
+      'dark' : '#333333',
+      'white' : '#fefefe',
+    },
   },
   variants: {
     extend: {},
