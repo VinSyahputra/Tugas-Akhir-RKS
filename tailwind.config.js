3c7cb6b02e053a4fa0e0d4cc7ef4f62e3944bddf
Vinsyahputra <vinthecyborg@gmail.com>
2020-12-13 13:38:35 +0700

    Home Page Mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,23 @@ module.exports = {
         'dark' : '#333333',
         'white' : '#fefefe',
       },
+      neumorphismColor: {
+        neo: {
+          'primary': '#E5F1F2',
+          'secondary': '#3C6AE1',
+          // ...
+        },
+      },
+      fontFamily: {
+        roboto: ['Roboto'],
+        posion: ['Poison'],
+        typoGrotesk : ['TypoGrotesk'],
+      },
     },
     
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('tailwindcss-neumorphism')],
 }
